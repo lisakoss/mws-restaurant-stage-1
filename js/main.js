b7915665bd2bf18d234cccc85b4a845962cd97ab
Lisa Koss <lisakoss@uw.edu>
2018-10-03 11:09:04 -0700

    feat: Change restaurant list links into buttons

--- js/main.js
+++ js/main.js
@@ -163,24 +163,29 @@ createRestaurantHTML = (restaurant) => {
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
+  const textArea = document.createElement('div');
+  textArea.classList.add('restaurant-lists-text-area');
+
   const name = document.createElement('h1');
   name.innerHTML = restaurant.name;
-  li.append(name);
+  textArea.append(name);
 
   const neighborhood = document.createElement('p');
   neighborhood.innerHTML = restaurant.neighborhood;
-  li.append(neighborhood);
+  textArea.append(neighborhood);
 
   const address = document.createElement('p');
   address.innerHTML = restaurant.address;
-  li.append(address);
+  textArea.append(address);
 
-  const more = document.createElement('a');
+  const more = document.createElement('button');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+  textArea.append(more);
 
-  return li
+  li.append(textArea);
+
+  return li;
 }
 
 /**
